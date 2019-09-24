44a21668b02260498d63f2e122df3161285365f4
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-09-24 07:37:30 +0700

    module kategori - css

--- module/kategori/list.php
+++ module/kategori/list.php
@@ -1,5 +1,5 @@
 <div id="frame-tambah">
-    <a href="<?php echo BASE_URL."index.php?page=my_profile&module=kategori&action=form"; ?>" class="tombol_action">+Tambah Kategori</a>
+    <a href="<?php echo BASE_URL."index.php?page=my_profile&module=kategori&action=form"; ?>" class="tombol-action">+Tambah Kategori</a>
 </div>
 
 <?php
@@ -10,21 +10,21 @@
         echo "<h3>Saat ini belum ada nama kategori didalam tabel Kategori</h3>";
     }else {
         echo "<table class='table-list'>";
-        echo "<tr>
-                <th>No</th>
-                <th>Kategori</th>
-                <th>Status</th>
-                <th>Action</th>
+        echo "<tr class='baris-title'>
+                <th class='kolom-nomor'>No</th>
+                <th class='kiri'>Kategori</th>
+                <th class='tengah'>Status</th>
+                <th class='tengah'>Action</th>
             </tr>";
 
         $no=1;
         while($row=mysqli_fetch_assoc($queryKategori)) {
             echo "<tr>
-                    <td>$no</td>
-                    <td>$row</td>
-                    <td>$row</td>
-                    <td>
-                        <a href='".BASE_URL."index.php?page=my_profile&module=kategori&action=formkategori_id=$row[kategori_id]'>Edit</a>
+                    <td class='kolom-nomor'>$no</td>
+                    <td class='kiri'>$row</td>
+                    <td class='tengah'>$row</td>
+                    <td class='tengah'>
+                        <a class='tombol-action' href='".BASE_URL."index.php?page=my_profile&module=kategori&action=formkategori_id=$row[kategori_id]'>Edit</a>
                     </td>
                 </tr>";
 
