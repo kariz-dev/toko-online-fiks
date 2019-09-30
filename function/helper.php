e901c519252eb58e123e067713bbbce137e34e10
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-01 04:39:50 +0700

    module barang - membuat fungsi rupiah

--- function/helper.php
+++ function/helper.php
@@ -1,3 +1,10 @@
 <?php
 
     define("BASE_URL", "http://localhost/fiks/");
+
+    // membuat fungsi Rupiah
+    function rupiah($nilai=0){
+        $string = "Rp ". number_format($nilai);
+        return $string;
+    }
+?>
