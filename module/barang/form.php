73a230fa96f0c16e5eb87e37abd473713c7101e1
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-01 03:59:46 +0700

    module barang - lanjut form

--- module/barang/form.php
+++ module/barang/form.php
@@ -1,7 +1,11 @@
 <?php
     $barang_id = isset($_GET['barang_id']) ? $_GET['barang_id'] : false;
 
-    // $kategori = "";
+    $nama_barang = "";
+    $spesifikasi = "";
+    $stok = "";
+    $harga = "";
+    $file = "";
     $status = "";
     $button = "Add";
 
@@ -15,20 +19,45 @@
     // }
 ?>
 
-<form action="<?php echo BASE_URL."module/barang/action.php?barang_id=$barang_id"; ?>" method="POST">
+<form action="<?php echo BASE_URL."module/barang/action.php?barang_id=$barang_id"; ?>" method="POST" enctype="multipart/form-data">
 
     <div class="element-form">
         <label for="">Kategori</label> 
         <span>
             <select name="kategori_id" >
                 <?php 
-                    $query = mysqli_query($koneksi, "SELECT * FROM kategori WHERE status='on'");
+                    $query = mysqli_query($koneksi, "SELECT kategori_id, kategori FROM kategori WHERE status='on' ORDER BY kategori ASC");
                     while($row=mysqli_fetch_assoc($query){
                         echo "<option value='$row[kategori_id]'>$row[kategori]</option>";
                     });
                 ?>
             </select>
         </span>
+    </div>
+
+    <div class="elemement-form">
+        <label for="">Nama Barang</label>
+        <span><input type="text" name="nama_barang" value="<?php echo $nama_barang; ?>" /></span>
+    </div>
+
+    <div class="element-form">
+        <label for="">Spesifikasi</label>
+        <span><textarea name="spesifikasi"><?php echo $spesifikasi; ?></textarea></span>
+    </div>
+
+    <div class="element-form">
+        <label for="">Stok Barang</label>
+        <span><input type="text" name="stok" value="<?php echo $stok; ?>" /></span>
+    </div>
+
+    <div class="element-form">
+        <label for="">Harga</label>
+        <span><input type="text" name="harga" value="<?php echo $harga; ?>" /></span>
+    </div>
+
+    <div class="element-form">
+        <label for="">Gambar Product</label>
+        <span><input type="file" name="file" /></span>
     </div>
 
     <div class="element-form">
