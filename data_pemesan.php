2be0280f7d47beb5675cb9d1f9a987d748f61e31
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-09 21:56:29 +0700

    proses pemesanan - detail order

--- data_pemesan.php
+++ data_pemesan.php
@@ -39,3 +39,41 @@
         </form>
     </div>
 </div>
+
+<div id="frame-data-detail">
+    <h3>Detail Order</h3>                 
+
+    <div id="frmae-detail-order">
+        <table class="table-list">
+            <tr>
+                <th class="kiri">Nama Barang</th>
+                <th class="tengah">Qty</th>
+                <th class="kanan">Total</th>
+            </tr>
+
+            <?php 
+                foreach($keranjang AS $key => $value){
+                    $barang_id = $key;
+
+                    $nama_barang = $value['nama_barang'];
+                    $harga = $value['harga'];
+                    $quantity = $value['quantity'];
+
+                    $total = $quantity * $harga;
+                    $subtotal = $subtotal + $total;
+
+                    echo <tr>
+                            <td class="kiri">$nama_barang</td>
+                            <td class="tengah">$quantity</td>
+                            <td class="kanan">".rupiah($total)."</td>
+                        </tr>;
+                }
+
+                echo <tr>
+                        <td class="kanan" colspan='2'><b>Subtotal</b></td>
+                        <td class="kanan"><b>".rupiah($subtotal)."</b></td>
+                    </tr>;
+            ?>
+        </table>
+    </div>
+</div>
