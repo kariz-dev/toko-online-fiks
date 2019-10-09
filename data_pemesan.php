2fc4b95c96df5ebaffc9f7ba4ee22fa31592b829
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-09 21:39:12 +0700

    proses pemesanan - form data pemesan

---
+++ data_pemesan.php
@@ -0,0 +1,41 @@
+<div id="frame-data-pengiriman">
+    <h3>Alamat Prngiriman Barang</h3>
+
+    <div id="frame-form-pengiriman">
+        <form action="<?php echo BASE_URL."proses_pemesanan.php"; ?>" method="POST">
+            <div class="element-form">
+                <label>Nama Penerima</label>
+                <span><input type="text" name="nama_penerima" /></span>
+            </div>
+
+            <div class="element-form">
+                <label>Nomor Telepon</label>
+                <span><input type="text" name="nomor_telepon" /></span>
+            </div>
+
+            <div class="element-form">
+                <label>Alamat pengiriman</label>
+                <span><textarea name="alamat"></textarea></span>
+            </div>
+
+            <div class="element-form">
+                <label>Kota</label>
+                <span>
+                    <select name="kota">
+                        <?php
+                            $query = mysqli_query($koneksi, "SELECT * FROM kota");
+
+                            while($row=mysqli_fetch_assoc($query)){
+                                echo "<option value='$row[kota_id]>$row[kota]</option>";
+                            }
+                        ?>
+                    </select>
+                </span>
+            </div>
+
+            <div class="element-form">
+                <span><input type="submit" value="submit" /></span>
+            </div>
+        </form>
+    </div>
+</div>
